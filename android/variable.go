4e829a0197ef7a4bf24eb2af9512d6c0e74b65e0
Danny Baumann <dannybaumann@web.de>
2018-08-29 11:04:58 +0200

    soong: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19
    Signed-off-by: hsj51 <hrutvikjagtap51@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -136,6 +136,10 @@ type variableProperties struct {
                         Shared_libs  []string
                 }
 
+		Target_process_sdk_version_override struct {
+			Cppflags []string
+		}
+
 		Target_shim_libs struct {
 			Cppflags []string
 		}
@@ -309,7 +313,7 @@ type productVariables struct {
 
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 	Has_legacy_camera_hal1 *bool `json:",omitempty"`
-
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs *string `json:",omitempty"`
 
         // include Lineage variables
